5c162f423c6079072830b426895e9eb098325180
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-07-24 21:11:29 +0400

    Initial frontend application

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld msg="It`s Works! Hello World" />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+body {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
