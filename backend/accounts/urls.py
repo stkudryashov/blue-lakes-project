a3515d219be852c3aa1d470f0d75e26cf82d6a8d
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-07-26 16:25:10 +0400

    Add simple JWT authentication

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,7 @@
+from django.urls import path
+
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import UserViewSet
 
@@ -6,5 +9,6 @@ router = routers.DefaultRouter()
 router.register(r'', UserViewSet, basename='accounts')
 
 urlpatterns = [
-
+  path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/v1/accounts/', include('accounts.urls'))
+    path('api/accounts/', include('accounts.urls'))
 ]
 
 admin.site.site_header = 'Blue Lakes CRM'
