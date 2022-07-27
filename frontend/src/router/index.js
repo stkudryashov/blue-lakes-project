f7b3bc16a36f5656acf9d957838c73ac0e2fb462
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-07-28 02:32:57 +0400

    Auth guard for router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
 import MainPage from '../pages/MainPage.vue'
 
+const authGuard = (to, from, next) => {
+  const isAuthorized = localStorage.getItem('token')
+
+  if (!isAuthorized) {
+    next({name: 'login'})
+  }
+  else {
+    next()
+  }
+}
+
 const routes = [
   {
     path: '/',
     name: 'main',
     component: MainPage,
+    beforeEnter: authGuard
   },
   {
     path: '/login',
